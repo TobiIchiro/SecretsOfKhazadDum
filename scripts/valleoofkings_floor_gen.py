bddd55ee936330cec9f5bb1817323bd96c7da8c9
Israel <israel.toledo@outlook.es>
2026-05-26 15:26:28 -0600

    Added Slopes

--- scripts/valleoofkings_floor_gen.py
+++ scripts/valleoofkings_floor_gen.py
@@ -131,29 +131,159 @@ FLOOR_TEMPLATE = {
         },
     ],
 }
+FLOOR_TEMPLATE_2 = {
+    "$type": "UAssetAPI.PropertyTypes.Structs.StructPropertyData, UAssetAPI",
+    "StructType": "GlobalLevelMeshInstance",
+    "SerializeNone": True,
+    "StructGUID": "{00000000-0000-0000-0000-000000000000}",
+    "SerializationControl": "NoExtension",
+    "Operation": "None",
+    "Name": "Instances",
+    "ArrayIndex": 0,
+    "IsZero": False,
+    "PropertyTagFlags": "None",
+    "PropertyTagExtensions": "NoExtension",
+    "Value": [
+        {
+            "$type": "UAssetAPI.PropertyTypes.Objects.NamePropertyData, UAssetAPI",
+            "Name": "Name",
+            "ArrayIndex": 0,
+            "IsZero": False,
+            "PropertyTagFlags": "None",
+            "PropertyTagExtensions": "NoExtension",
+            "Value": "Suburbs_Gate_A_Pillar_Shaft1",
+        },
+        {
+            "$type": "UAssetAPI.PropertyTypes.Structs.StructPropertyData, UAssetAPI",
+            "StructType": "Transform",
+            "SerializeNone": True,
+            "StructGUID": "{00000000-0000-0000-0000-000000000000}",
+            "SerializationControl": "NoExtension",
+            "Operation": "None",
+            "Name": "Transform",
+            "ArrayIndex": 0,
+            "IsZero": False,
+            "PropertyTagFlags": "None",
+            "PropertyTagExtensions": "NoExtension",
+            "Value": [
+                {
+                    "$type": "UAssetAPI.PropertyTypes.Structs.StructPropertyData, UAssetAPI",
+                    "StructType": "Quat",
+                    "SerializeNone": True,
+                    "StructGUID": "{00000000-0000-0000-0000-000000000000}",
+                    "SerializationControl": "NoExtension",
+                    "Operation": "None",
+                    "Name": "Rotation",
+                    "ArrayIndex": 0,
+                    "IsZero": False,
+                    "PropertyTagFlags": "None",
+                    "PropertyTagExtensions": "NoExtension",
+                    "Value": [
+                        {
+                            "$type": "UAssetAPI.PropertyTypes.Structs.QuatPropertyData, UAssetAPI",
+                            "Name": "Rotation",
+                            "ArrayIndex": 0,
+                            "IsZero": False,
+                            "PropertyTagFlags": "None",
+                            "PropertyTagExtensions": "NoExtension",
+                            "Value": {
+                                "$type": "UAssetAPI.UnrealTypes.FQuat, UAssetAPI",
+                                "X": "+0",
+                                "Y": "+0",
+                                "Z": 1.0,
+                                "W": 0.0,
+                            },
+                        }
+                    ],
+                },
+                {
+                    "$type": "UAssetAPI.PropertyTypes.Structs.StructPropertyData, UAssetAPI",
+                    "StructType": "Vector",
+                    "SerializeNone": True,
+                    "StructGUID": "{00000000-0000-0000-0000-000000000000}",
+                    "SerializationControl": "NoExtension",
+                    "Operation": "None",
+                    "Name": "Translation",
+                    "ArrayIndex": 0,
+                    "IsZero": False,
+                    "PropertyTagFlags": "None",
+                    "PropertyTagExtensions": "NoExtension",
+                    "Value": [
+                        {
+                            "$type": "UAssetAPI.PropertyTypes.Structs.VectorPropertyData, UAssetAPI",
+                            "Name": "Translation",
+                            "ArrayIndex": 0,
+                            "IsZero": False,
+                            "PropertyTagFlags": "None",
+                            "PropertyTagExtensions": "NoExtension",
+                            "Value": {
+                                "$type": "UAssetAPI.UnrealTypes.FVector, UAssetAPI",
+                                "X": 1750.0,
+                                "Y": 7450.0,
+                                "Z": 800.0,
+                            },
+                        }
+                    ],
+                },
+                {
+                    "$type": "UAssetAPI.PropertyTypes.Structs.StructPropertyData, UAssetAPI",
+                    "StructType": "Vector",
+                    "SerializeNone": True,
+                    "StructGUID": "{00000000-0000-0000-0000-000000000000}",
+                    "SerializationControl": "NoExtension",
+                    "Operation": "None",
+                    "Name": "Scale3D",
+                    "ArrayIndex": 0,
+                    "IsZero": False,
+                    "PropertyTagFlags": "None",
+                    "PropertyTagExtensions": "NoExtension",
+                    "Value": [
+                        {
+                            "$type": "UAssetAPI.PropertyTypes.Structs.VectorPropertyData, UAssetAPI",
+                            "Name": "Scale3D",
+                            "ArrayIndex": 0,
+                            "IsZero": False,
+                            "PropertyTagFlags": "None",
+                            "PropertyTagExtensions": "NoExtension",
+                            "Value": {
+                                "$type": "UAssetAPI.UnrealTypes.FVector, UAssetAPI",
+                                "X": 1.0,
+                                "Y": 1.0,
+                                "Z": 1.0,
+                            },
+                        }
+                    ],
+                },
+            ],
+        },
+    ],
+}
 
 with open(PATH_FILE, "r", encoding="utf-8") as f:
     data_a = json.load(f)
 
-floors = data_a["Exports"][0]["Data"][0]["Value"][0]["Value"][92]["Value"][1]["Value"]
+floors = data_a["Exports"][0]["Data"][0]["Value"][0]["Value"][2]["Value"][1]["Value"]
 k = 0
-for i in range(0, 12):
+z = 900.0
+y = 8300.0
+for i in range(0, 5):
     a = copy.deepcopy(FLOOR_TEMPLATE)
-    a["Value"][0]["Value"] = f"Suburbs_Gate_A_Pillar_Shaft{i+1}"
-    z = 1100 + 100.0 * k
-    if i % 4 == 0:
-        x = 1100.0
-        y = 7100.0
-    elif i % 4 == 1:
-        x = 400.0
-        y = 7100.0
-    elif i % 4 == 2:
-        x = 1100.0
-        y = 8100.0
-    else:
-        x = 400.0
-        y = 8100.0
-        k += 1
+    a["Value"][0]["Value"] = f"SM_AR_Suburbs_Slope2_200x300x200_A{i+1}"
+    x = 1450 - 300 * i
+
+
+    a["Value"][1]["Value"][1]["Value"][0]["Value"]["X"] = x
+    a["Value"][1]["Value"][1]["Value"][0]["Value"]["Y"] = y
+    a["Value"][1]["Value"][1]["Value"][0]["Value"]["Z"] = z
+
+    floors.append(a)
+
+y = 6900.0
+for i in range(0, 5):
+    a = copy.deepcopy(FLOOR_TEMPLATE_2)
+    a["Value"][0]["Value"] = f"SM_AR_Suburbs_Slope2_200x300x200_A{i+7}"
+    x = 1450 - 300 * i
+
 
     a["Value"][1]["Value"][1]["Value"][0]["Value"]["X"] = x
     a["Value"][1]["Value"][1]["Value"][0]["Value"]["Y"] = y
